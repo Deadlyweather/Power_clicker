de3f0e87eb44a96339660e90b2b90ef0ecbf62a3
Tony Ruotsalainen <tonyruotsalainenuusi@gmail.com>
2025-11-05 11:56:34 +0200

    Paljon päivityksiä Triolle

--- Koodi/Koodi.js
+++ Koodi/Koodi.js
@@ -22,12 +22,35 @@ collector.addEventListener('click', () => {
 
 // Sähkö Kauppa
 const sahkoKauppa = document.getElementById('SahkoKauppa');
-sahkoKauppa.hidden = true; // Asetetaan kauppa aluksi piiloon
+const collectorKauppa = document.getElementById('CollectorKauppa');
+const tukiKauppa = document.getElementById('TukiKauppa');
 
 function AvaaSahkoKauppa() {
     if (sahkoKauppa.hidden === true) {
         sahkoKauppa.hidden = false;
+        collectorKauppa.hidden = true;
+        tukiKauppa.hidden = true;
     } else {
         sahkoKauppa.hidden = true;
+    }
+}
+
+function AvaaCollectorKauppa() {
+    if (collectorKauppa.hidden === true) {
+        collectorKauppa.hidden = false;
+        sahkoKauppa.hidden = true;
+        tukiKauppa.hidden = true;
+    } else {
+        collectorKauppa.hidden = true;
+    }
+}
+
+function AvaaTukiKauppa() {
+    if (tukiKauppa.hidden === true) {
+        tukiKauppa.hidden = false;
+        collectorKauppa.hidden = true;
+        sahkoKauppa.hidden = true;
+    } else {
+        tukiKauppa.hidden = true;
     }
 }
